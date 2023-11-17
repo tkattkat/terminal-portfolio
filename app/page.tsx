14c7b032b9da2ed66a0bc9dce625e98aeac53c56
tkattkat <tkat@tkat.net>
2023-11-16 19:06:51 -0800

    portfolio terminal

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,82 @@
-import Image from 'next/image'
+'use client'
+import { useState } from 'react';
 
-export default function Home() {
+export default function Component() {
+  const [inputCommand, setInputCommand] = useState('');
+  const [commandHistory, setCommandHistory] = useState([
+    {text: 'Welcome to the Portfolio Terminal', color: 'text-green-500'},
+    {text: "Type '/help' to navigate through my professional journey and accomplishments", color: 'text-yellow-500'},
+    {text: 'Enjoy your time here!', color: 'text-blue-500'},
+  ]);
+
+  // List of commands and their descriptions
+  const commands = {
+    'about': 'about me',
+    'portfolio': 'List of past and present projects',
+    'github': 'Link to github',
+    // Add more commands and descriptions here 
+  };
+
+  // Command responses
+  const commandResponses = {
+    'about': 'This is a brief paragraph about me. [Insert your bio here].',
+    'portfolio': [
+      { name: 'Project 1', url: 'https://link-to-project-1.com' },
+      { name: 'Project 2', url: 'https://link-to-project-2.com' },
+      // Add more projects as needed
+    ],
+    'github': 'https://github.com/example',
+  };
+
+  // Handle command input
+  const handleCommandInput = (command:string) => {
+    setInputCommand('');
+
+    // Update command history with the entered command
+    setCommandHistory(prevHistory => [...prevHistory, {text: `$ ${command}`, color: 'text-green-300'}]);
+
+    if (command.trim() === '/help') {
+      const helpMessage = Object.entries(commands).map(
+        ([cmd, description]) => ({text: `$ ${cmd} - ${description}`, color: 'text-green-300'})
+      );
+      setCommandHistory(prevHistory => [...prevHistory, ...helpMessage]);
+    } else if (command.trim() === 'about') {
+      setCommandHistory(prevHistory => [...prevHistory, {text: commandResponses.about, color: 'text-green-300'}]);
+    } else if (command.trim() === 'portfolio') {
+      commandResponses.portfolio.forEach(project => {
+        setCommandHistory(prevHistory => [...prevHistory, {text: `${project.name} - ${project.url}`, color: 'text-green-300'}]);
+      });
+    } else if (command.trim() === 'github') {
+      setCommandHistory(prevHistory => [...prevHistory, {text: commandResponses.github, color: 'text-green-300'}]);
+    } else {
+      setCommandHistory(prevHistory => [...prevHistory, {text: 'Unknown command. Enter /help for a list of commands.', color: 'text-red-500'}]);
+    }
+  };
+
+  // Handle command submission
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    handleCommandInput(inputCommand);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <section className="w-full h-screen bg-black text-white p-8">
+      {commandHistory.map((line, index) => (
+        <pre key={index} className={`font-mono ${line.color} mt-2`}>
+          <code>{`$ ${line.text}`}</code>
+        </pre>
+      ))}
+      <div className="h-0.5 bg-gray-800 my-4" />
+      <form onSubmit={handleSubmit} className="font-mono text-white flex items-center">
+        <code>{`$ >_`}</code>
+        <input
+          aria-label="Terminal Input"
+          className="bg-black text-white ml-2 outline-none focus:outline-none"
+          type="text"
+          value={inputCommand}
+          onChange={(e) => setInputCommand(e.target.value)}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </form>
+    </section>
+  );
 }
